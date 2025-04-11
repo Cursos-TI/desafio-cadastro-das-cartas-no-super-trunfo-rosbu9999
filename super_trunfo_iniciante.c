bcb5e457ea66698210718f38b704a1a272842d0d
rosbu9999 <andersonmota721@gmail.com>
2025-04-11 17:14:24 +0000

    fiz uma correção e adicionei o registro da segunda carta

--- super_trunfo_iniciante.c
+++ super_trunfo_iniciante.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
 
-    char estado;
+    char estado[20];
     char codigo[20];
     char cidade[30];
     int populacao;
     float area;
     float pib;
     int pontosturisticos;
+
+    printf("carta 01\n");
 
     printf("digite o nome do estado: ");
     scanf("%s", &estado);
@@ -29,7 +31,39 @@ int main(){
     printf("Codigo: %s \n", codigo);
     printf("Cidade: %s \n", cidade);
     printf("População: %d \n", populacao);
-    printf("Área: %f \n", area);
-    printf("Pib: %f \n", pib);
+    printf("Área: %.2f \n", area);
+    printf("Pib: %.2f \n", pib);
     printf("Pontos Turisticos: %d \n", pontosturisticos);
+
+    printf("carta 02\n");
+
+    printf("digite o nome do estado: ");
+    scanf("%s", &estado);
+    printf("digite o codigo da carta: ");
+    scanf("%s", &codigo);
+    printf("digite o nome da cidade: ");
+    scanf("%s", &cidade);
+    printf("digite o numero de habitantes: ");
+    scanf("%d", &populacao);
+    printf("digite a área da cidade: ");
+    scanf("%f", &area);
+    printf("digite o pib da cidade: ");
+    scanf("%f", &pib);
+    printf("digite a quantidade de pontos turisticos: \n ");
+    scanf("%d", &pontosturisticos);
+
+    printf("carta 02\n");
+
+    printf("estado: %s\n", estado);
+    printf("Codigo: %s \n", codigo);
+    printf("Cidade: %s \n", cidade);
+    printf("População: %d \n", populacao);
+    printf("Área: %.2f \n", area);
+    printf("Pib: %.2f \n", pib);
+    printf("Pontos Turisticos: %d \n", pontosturisticos);
+
+
+    return 0;
+
+
 }
