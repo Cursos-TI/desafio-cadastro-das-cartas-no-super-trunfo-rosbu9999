2894df8e631f706d8bbdf1b12000ad36610c0a97
rosbu9999 <andersonmota721@gmail.com>
2025-04-11 01:36:56 +0000

    corrigi oque estava errado nos testes

--- cartas_super_trunfo_iniciante.c
+++ cartas_super_trunfo_iniciante.c
@@ -4,7 +4,7 @@ int main(){
     char estado;
     char codigo[20];
     char cidade[30];
-    int população;
+    int populacao;
     float area;
     float pib;
     int pontos_turisticos;
@@ -12,16 +12,16 @@ int main(){
     printf("digite o nome do estado: \n");
     scanf("%s", &estado);
     printf("digite o codigo da carta: \n");
-    scan("%s", &codigo);
+    scanf("%s", &codigo);
     printf("digite o nome da cidade: \n");
     scanf("%s", &cidade);
     printf("digite o numero de habitantes: \n");
-    scanf("%d", população);
+    scanf("%d", &populacao);
     printf("digite a área da cidade: \n");
     scanf("%f", &area);
     printf("digite o pib da cidade: \n");
     scanf("%f", &pib);
-    printf("digite a quantidade de pontos turisticos: \n")
+    printf("digite a quantidade de pontos turisticos: \n");
 
 
 
