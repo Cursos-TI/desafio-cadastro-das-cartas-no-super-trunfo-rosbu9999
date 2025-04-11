f06b206ace5dc01affab8d24784459efc55c780c
rosbu9999 <andersonmota721@gmail.com>
2025-04-11 01:05:32 +0000

    coloquei as variaeis

---
+++ cartas_super_trunfo_iniciante.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    char estado;
+    char codigo[20];
+    char cidade[30];
+    int população;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    
+
+
+
+
+
+}
